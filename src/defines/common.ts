4347cb7a4085d24fd62072b3ad5a9d9970daedc1
DangDong204 <165341409+DangDong204@users.noreply.github.com>
2026-01-12 14:50:22 +0700

    refactor: move project react into folder frontend & add backend

--- src/defines/common.ts
+++
@@ -1,5 +0,0 @@
-export const Themes = {
-  DARK: 'dark',
-  LIGHT: 'light'
-} as const
-export type Themes = (typeof Themes)[keyof typeof Themes]
--- src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-declare module 'virtual:i18next-loader' {
-  import type { Resource } from 'i18next'
-  const resources: Resource
-  export default resources
-}
